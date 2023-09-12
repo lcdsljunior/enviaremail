5145cf9e10c6d78a6f839c6072b5effd48649a89
Luiz Junior <luizjunior.ti@gmail.com>
2023-09-12 11:16:30 -0300

    Primeiro passos

---
+++ enviaremail/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace enviaremail
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtDestinatario = new System.Windows.Forms.TextBox();
+            this.txtAssunto = new System.Windows.Forms.TextBox();
+            this.txtMensagem = new System.Windows.Forms.TextBox();
+            this.lblAnexo = new System.Windows.Forms.Label();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Destinatario";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Assunto";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Mensagem";
+            // 
+            // txtDestinatario
+            // 
+            this.txtDestinatario.Location = new System.Drawing.Point(93, 32);
+            this.txtDestinatario.Multiline = true;
+            this.txtDestinatario.Name = "txtDestinatario";
+            this.txtDestinatario.Size = new System.Drawing.Size(439, 20);
+            this.txtDestinatario.TabIndex = 5;
+            // 
+            // txtAssunto
+            // 
+            this.txtAssunto.Location = new System.Drawing.Point(93, 62);
+            this.txtAssunto.Name = "txtAssunto";
+            this.txtAssunto.Size = new System.Drawing.Size(439, 20);
+            this.txtAssunto.TabIndex = 6;
+            // 
+            // txtMensagem
+            // 
+            this.txtMensagem.Location = new System.Drawing.Point(93, 97);
+            this.txtMensagem.Multiline = true;
+            this.txtMensagem.Name = "txtMensagem";
+            this.txtMensagem.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtMensagem.Size = new System.Drawing.Size(439, 91);
+            this.txtMensagem.TabIndex = 7;
+            // 
+            // lblAnexo
+            // 
+            this.lblAnexo.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblAnexo.Location = new System.Drawing.Point(93, 198);
+            this.lblAnexo.Name = "lblAnexo";
+            this.lblAnexo.Size = new System.Drawing.Size(439, 23);
+            this.lblAnexo.TabIndex = 8;
+            this.lblAnexo.Click += new System.EventHandler(this.lblAnexo_Click);
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(93, 230);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(439, 43);
+            this.btnEnviar.TabIndex = 9;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(31, 202);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Anexo";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(562, 303);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.lblAnexo);
+            this.Controls.Add(this.txtMensagem);
+            this.Controls.Add(this.txtAssunto);
+            this.Controls.Add(this.txtDestinatario);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtDestinatario;
+        private System.Windows.Forms.TextBox txtAssunto;
+        private System.Windows.Forms.TextBox txtMensagem;
+        private System.Windows.Forms.Label lblAnexo;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ enviaremail/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Mail;
+using System.Net;
+
+namespace enviaremail
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            EmailEnviar();
+        }
+
+        private void LimparCampos()
+        {
+            
+            txtDestinatario.Text = string.Empty;
+            txtAssunto.Text = string.Empty;
+            txtMensagem.Text = string.Empty;
+            lblAnexo.Text = string.Empty;
+            txtDestinatario.Focus();
+           
+        }
+
+        private void EmailEnviar()
+        {
+            try
+            {
+                using (SmtpClient smtp = new SmtpClient())
+                {
+                    using (MailMessage email = new MailMessage())
+                    {
+                        //Servidor SMTP
+                        smtp.Host = "smtpi.valebuscard.com.br";
+                        smtp.UseDefaultCredentials = false;
+                        smtp.Credentials = new NetworkCredential("luiz.carlos@valebuscard.com.br", "Lu1z#C4rl0s");
+                        smtp.Port = 587;
+                        smtp.EnableSsl = false;
+
+                        //Email (Mensagem)
+                        email.From = new MailAddress("luiz.carlos@valebuscard.com.br");
+                        email.To.Add(txtDestinatario.Text);
+
+                        //Mandar Email
+                        email.Subject = txtAssunto.Text;
+                        email.IsBodyHtml = true;
+                        //email.Body = "<html>\r\n<head>\r\n    <title>Template</title>\r\n</head>\r\n<body>\r\n    <table>\r\n        <tr>\r\n            <td>\r\n                <img src=\"https://sitranscg.com.br/wp-content/uploads/img/cliente3.png\" alt=\"\" />\r\n            </td>\r\n        </tr>\r\n        <tr>\r\n            <td>\r\n                ##Mensagem##\r\n            </td>\r\n        </tr>\r\n        <tr>\r\n            <td>\r\n                Cesar Cassiano Schimanco\r\n                <a href=\"http://www.cbsa.com.br\">www.cbsa.com.br</a>\r\n            </td>\r\n        </tr>\r\n    </table>\r\n</body>\r\n</html>";
+                        email.Body = "<html>" +
+                                      "<body>" +
+                                      "<table>" +
+                                      "<tr>" +
+                                      "<td>" +
+                                      "Uma homenagem aos melhores clientes." +
+                                      "</td>" +
+                                      "</tr>" +
+                                      "<tr>" +
+                                      "<td>" +
+                                      "<img src=\"https://www.sitranscg.com.br/wp-content//uploads/img/cliente3.png\" alt=\"\" />" +
+                                      "</td>" +
+                                      "</tr>" +
+                                      " </table>" +
+                                      "</body>" +
+                                      "</html>";
+                       
+                        //email.Body = txtMensagem.Text;
+
+                        if (lblAnexo.Text != "")
+                        {
+                            var anexo = lblAnexo.Text.ToString().Split(';');
+                            for (int i = 0; i < anexo.Count(); i++)
+                                email.Attachments.Add(new Attachment(anexo[i]));
+                        }
+
+
+                        //Enviar email
+                        smtp.Send(email);
+
+                    }
+                }
+                LimparCampos();
+                MessageBox.Show("Email enviado.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro: " + ex.Message);
+            }
+        }
+
+        private void lblAnexo_Click(object sender, EventArgs e)
+        {
+            var anexo = new OpenFileDialog();
+            
+            anexo.Multiselect = true;
+            anexo.Title = "Anexar Arquivo";
+
+            if (anexo.ShowDialog() == DialogResult.OK)
+                for (int i = 0; i < anexo.FileNames.Count(); i++)
+                    if (i == 0)
+                lblAnexo.Text = anexo.FileNames[i];
+            else
+                lblAnexo.Text = lblAnexo.Text + ";" + anexo.FileNames[i];
+        }
+    }
+}
